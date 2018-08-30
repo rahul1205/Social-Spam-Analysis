3160750bf5718666a380cf43a87da6393ba56c9f
rahul1205 <anand.rahul1205@gmail.com>
2018-08-30 18:15:18 +0530

    minor api tweaks

--- twitter/api.py
+++ twitter/api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import re
 
 from rest_framework import generics
 from twitter.models import Twitter
@@ -11,12 +12,15 @@ class GetTweets(generics.ListAPIView):
     model = Twitter
 
     def get_queryset(self):
+        cleaned_number = None
         queryset = self.model.objects.none()
         input_number = self.request.query_params.get('number')
         if input_number:
+            cleaned_number = re.sub('[ -/A-z]', '', input_number)
+        if cleaned_number and len(cleaned_number) > 6:
             try:
                 queryset = Twitter.objects.filter(
-                    content__icontains=input_number)
+                    numbers__icontains=cleaned_number)
             except:
                 queryset = self.model.objects.none()
         return queryset
