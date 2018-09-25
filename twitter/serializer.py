406e07baa4e3cc0d52c97c6f1da3f5874b7d467f
rahul1205 <anand.rahul1205@gmail.com>
2018-09-25 15:08:55 +0530

    Final cut with tempelate

--- twitter/serializer.py
+++ twitter/serializer.py
@@ -4,9 +4,26 @@ from __future__ import unicode_literals
 from rest_framework import serializers
 
 from twitter.models import Twitter
+from geopy.geocoders import Nominatim
 
 
 class TwitterSerializer(serializers.ModelSerializer):
+    latitude = serializers.SerializerMethodField(read_only=True)
+    longitude = serializers.SerializerMethodField(read_only=True)
+
+    def get_latitude(self, obj):
+        geolocator = Nominatim(user_agent="SSA")
+        if not obj.location == "-1":
+            if geolocator.geocode(obj.location).latitude:
+                return geolocator.geocode(obj.location).latitude
+        return 28.6139
+
+    def get_longitude(self, obj):
+        geolocator = Nominatim(user_agent="SSA")
+        if not obj.location == "-1":
+            if geolocator.geocode(obj.location).longitude:
+                return geolocator.geocode(obj.location).longitude
+        return 77.2090
 
     class Meta:
         model = Twitter
